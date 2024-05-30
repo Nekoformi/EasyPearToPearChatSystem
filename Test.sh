d2dc69c8990067cfe79cd4db8709685656f3ed28
Nekoformi <>
2024-05-30 11:59:46 +0900

    Update environment

--- Test.sh
+++ Test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-java -jar E=CS.jar -n="Test User A" -x=0 -y=60 -t=5000 -create=20000 -ssl &
-java -jar E=CS.jar -n="Test User B" -x=640 -y=60 -t=5000 -ssl &
-java -jar E=CS.jar -n="Test User C" -x=1280 -y=60 -t=5000 -ssl &
-java -jar E=CS.jar -n="Test User D" -x=0 -y=540 -t=5000 -ssl &
-java -jar E=CS.jar -n="Test User E" -x=640 -y=540 -t=5000 -ssl &
-java -jar E=CS.jar -n="Test User F" -x=1280 -y=540 -t=5000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User A" -x=0 -y=60 -t=5000 -create=20000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User B" -x=640 -y=60 -t=5000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User C" -x=1280 -y=60 -t=5000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User D" -x=0 -y=540 -t=5000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User E" -x=640 -y=540 -t=5000 -ssl &
+java -jar ./Test/E=CS.jar -n="Test User F" -x=1280 -y=540 -t=5000 -ssl &
